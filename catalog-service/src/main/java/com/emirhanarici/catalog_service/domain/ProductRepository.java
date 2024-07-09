207b4ccb461bb6511eadda096d5042ea8c059f26
emirhanaricii <emirhan190341@gmail.com>
2024-07-09 16:30:49 +0300

    test classes and exception handling added

--- catalog-service/src/main/java/com/emirhanarici/catalog_service/domain/ProductRepository.java
+++ catalog-service/src/main/java/com/emirhanarici/catalog_service/domain/ProductRepository.java
@@ -2,5 +2,8 @@ package com.emirhanarici.catalog_service.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    Optional<ProductEntity> findByCode(String code);
 }
--- catalog-service/src/main/java/com/emirhanarici/catalog_service/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/emirhanarici/catalog_service/web/controllers/ProductController.java
@@ -2,15 +2,17 @@ package com.emirhanarici.catalog_service.web.controllers;
 
 import com.emirhanarici.catalog_service.domain.PagedResult;
 import com.emirhanarici.catalog_service.domain.Product;
+import com.emirhanarici.catalog_service.domain.ProductNotFoundException;
 import com.emirhanarici.catalog_service.domain.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
 class ProductController {
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
     private final ProductService productService;
 
     ProductController(ProductService productService) {
@@ -21,4 +23,15 @@ class ProductController {
     PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
         return productService.getProducts(pageNo);
     }
+
+    @GetMapping("/{code}")
+    ResponseEntity<Product> getProductByCode(@PathVariable String code) {
+        log.info("Fetching product for code: {}", code);
+        return productService
+                .getProductByCode(code)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> ProductNotFoundException.forCode(code));
+    }
+
+
 }
