0f49a2ea56c2f22854267cce38cb13d353029383
emirhanaricii <emirhan190341@gmail.com>
2024-07-11 13:18:47 +0300

    initialize orderservice project

---
+++ order-service/src/main/java/com/emirhanarici/order_service/domain/OrderNotFoundException.java
@@ -0,0 +1,12 @@
+package com.emirhanarici.order_service.domain;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public static OrderNotFoundException forOrderNumber(String orderNumber) {
+        return new OrderNotFoundException("Order with Number  " + orderNumber + " not found");
+    }
+}
