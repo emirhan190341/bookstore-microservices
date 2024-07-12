93ccb0449d4007b11a9842234b9122e5054c5e69
emirhanaricii <emirhan190341@gmail.com>
2024-07-12 23:07:28 +0300

    Publish order events to rabbitmq with using outbox pattern

---
+++ order-service/src/main/java/com/emirhanarici/order_service/domain/OrderEventRepository.java
@@ -0,0 +1,6 @@
+package com.emirhanarici.order_service.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderEventRepository extends JpaRepository<OrderEventEntity,Long> {
+}
--- order-service/src/main/java/com/emirhanarici/order_service/domain/OrderRepository.java
+++ order-service/src/main/java/com/emirhanarici/order_service/domain/OrderRepository.java
@@ -1,6 +1,20 @@
 package com.emirhanarici.order_service.domain;
 
+import com.emirhanarici.order_service.domain.models.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    List<OrderEntity> findByStatus(OrderStatus status);
+    Optional<OrderEntity> findByOrderNumber(String orderNumber);
+
+    default void updateOrderStatus(String orderNumber, OrderStatus status) {
+        OrderEntity order = this.findByOrderNumber(orderNumber).orElseThrow();
+        order.setStatus(status);
+        this.save(order);
+    }
+
+
 }
