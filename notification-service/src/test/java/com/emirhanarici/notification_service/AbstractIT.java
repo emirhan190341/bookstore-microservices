156418357a5a4e441832f2cc48d0287a48445d13
emirhanaricii <emirhan190341@gmail.com>
2024-07-16 23:58:41 +0300

    implementing and dockerization of notification service

---
+++ notification-service/src/test/java/com/emirhanarici/notification_service/AbstractIT.java
@@ -0,0 +1,15 @@
+package com.emirhanarici.notification_service;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Import(ContainersConfig.class)
+public abstract class AbstractIT {
+    @MockBean
+    protected NotificationServiceApplication notificationService;
+}
---
+++ notification-service/src/test/java/com/emirhanarici/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emirhanarici.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class NotificationServiceApplicationTests extends AbstractIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
